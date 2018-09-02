179dc0df1a23b758570d0883c4397332259fa183
miller <yuhaim>
2018-09-02 13:12:54 +0800

    add some comments

---
+++ target1/part_1_level_2.d
@@ -0,0 +1,11 @@
+
+part_1_level_2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	bf fa 97 b9 59       	mov    $0x59b997fa,%edi
+   5:	48 c7 c0 ec 17 40 00 	mov    $0x4017ec,%rax
+   c:	48 89 04 24          	mov    %rax,(%rsp)
+  10:	c3                   	retq   
---
+++ target1/part_1_level_3.d
@@ -0,0 +1,11 @@
+
+part_1_level_3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 b0 dc 61 55 	mov    $0x5561dcb0,%rdi
+   7:	48 c7 c0 fa 18 40 00 	mov    $0x4018fa,%rax
+   e:	48 89 04 24          	mov    %rax,(%rsp)
+  12:	c3                   	retq   
